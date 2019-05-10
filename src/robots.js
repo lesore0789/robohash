efde2ff4e03a7e1c333d3ba3176e7ebf4928e5eb
lesore0789 <lesore0789@gmail.com>
2019-05-09 22:10:49 -0500

    Use API fetch of users instead of file

--- src/robots.js
+++ src/robots.js
@@ -31,7 +31,7 @@ export const robots = [
   },
   {
     id: 6,
-    name: 'Mrs. Dennis Schulist',
+    name: 'Dennis Schulist',
     username: 'Leopoldo_Corkery',
     email: 'Karley_Dach@jasper.info'
   },
@@ -43,7 +43,7 @@ export const robots = [
   },
   {
     id: 8,
-    name: 'Nicholas Runolfsdottir V',
+    name: 'Nicholas Runolf',
     username: 'Maxime_Nienow',
     email: 'Sherwood@rosamond.me'
   },
